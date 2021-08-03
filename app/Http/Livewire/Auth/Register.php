75a2bc0aaf5b21c29801785f49e40f931360b996
brandon-leaprice <brandonpricw@scolemore.com>
2021-08-03 12:03:15 +0100

    Added basic Register

--- app/Http/Livewire/Auth/Register.php
+++ app/Http/Livewire/Auth/Register.php
@@ -2,10 +2,28 @@
 
 namespace App\Http\Livewire\Auth;
 
+use App\Models\User;
 use Livewire\Component;
 
 class Register extends Component
 {
+
+    public $name;
+    public $email;
+    public $password;
+    public $passwordConfirmation;
+
+
+    public function register()
+    {
+        User::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => \Hash::make($this->password),
+        ]);
+    }
+
+
     public function render()
     {
         return view('livewire.auth.register')
--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -1,3 +1,10 @@
 <div>
-   Hello Registration
+    <form wire:submit.prevent="register">
+        <input wire:model="name" type="text">
+        <input wire:model="email" type="text">
+        <input wire:model="password" type="password">
+        <input wire:model="passwordConfirmation" type="password">
+
+        <button type="submit">Register</button>
+    </form>
 </div>
